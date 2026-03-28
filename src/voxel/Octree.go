704f9f2eb6e22e6eab437bd79ee350650d23ee2e
SlackingSlothh <zkyrbbn@gmail.com>
2026-03-28 12:24:20 +0700

    feat: early return and exported field

--- src/voxel/Octree.go
+++ src/voxel/Octree.go
@@ -3,28 +3,42 @@ package voxel
 import "github.com/SlackingSlothh/OctreeVoxelization/src/geometry"
 
 type OctreeNode struct {
-	boundary geometry.Cube
-	triangles []geometry.Triangle3D
-	children []OctreeNode
+	Boundary geometry.Cube
+	Triangles []geometry.Triangle3D
+	Children []OctreeNode
 }
 
-const VOXEL_SIZE = 1.0
+const MAX_DEPTH = 6
+const MIN_EDGE = 0.005
 
-func (node OctreeNode) Construct() {
-	if len(node.triangles) == 0 {
-		node.children = nil
-	} else if node.boundary.GetEdgeLength() <= VOXEL_SIZE {
-		node.children = nil
-	} else {
-		dividedCube := node.boundary.DivideCube()
-		for _, childCube := range dividedCube {
-			childNode := OctreeNode{childCube, nil, nil}
-			for _, triangle := range node.triangles {
-				if triangle.IsIntersecting(childCube) {
-					childNode.triangles = append(childNode.triangles, triangle)
-				}
-			}
-			childNode.Construct()
-		}
+func (node *OctreeNode) Construct(currentDepth int) int {
+	if len(node.Triangles) == 0 {
+		node.Children = nil
+		return currentDepth
 	}
+	if currentDepth >= MAX_DEPTH || node.Boundary.GetEdgeLength() <= MIN_EDGE {
+		node.Children = nil
+		return currentDepth
+	}
+
+	node.Children = nil
+	dividedCube := node.Boundary.DivideCube()
+	maxDepth := currentDepth
+	for _, childCube := range dividedCube {
+		childNode := OctreeNode{Boundary: childCube}
+		for _, triangle := range node.Triangles {
+			if triangle.IsIntersecting(childCube) {
+				childNode.Triangles = append(childNode.Triangles, triangle)
+			}
+		}
+		if len(childNode.Triangles) == 0 {
+			continue
+		}
+		childDepth := childNode.Construct(currentDepth + 1)
+		if childDepth > maxDepth {
+			maxDepth = childDepth
+		}
+		node.Children = append(node.Children, childNode)
+	}
+	return maxDepth
 }
